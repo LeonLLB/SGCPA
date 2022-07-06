0f251a3bd3c81b470dc34a3c493e8d532bbac581
Leonard Velásquez <leonard.avp@gmail.com>
2022-07-06 18:12:48 -0400

    Formulario de inscripcion

--- pages/presentaciones/index.tsx
+++ pages/presentaciones/index.tsx
@@ -1,3 +1,5 @@
+import prisma from "../../prismaClient"
+
 const PresentacionesMain = () => {
   return (
     <div>PresentacionesMain</div>
@@ -5,3 +7,37 @@ const PresentacionesMain = () => {
 }
 
 export default PresentacionesMain
+
+export const getServerSideProps = async (context) =>{
+
+  const ultimaCarga = await prisma.cargaAcademica.findFirst({
+    orderBy:{
+      periodo:'desc'
+    },
+    select:{
+      periodo:true
+    }
+  })
+
+  const listadoCargas = await prisma.cargaAcademica.findMany({
+    include:{
+      docente:true
+    },
+    orderBy:[
+      {pnf:'desc'},
+      {trayecto:'asc'}
+    ],
+    where:{
+      periodo:ultimaCarga.periodo
+    }
+  })
+  const listadoDocentes = await prisma.docente.findMany()
+  const listadoPNF = await prisma.pNF.findMany()
+   return {
+       props: {
+        listado:listadoCargas,
+        listadoDocentes,
+        listadoPNF
+        }
+    }
+}
