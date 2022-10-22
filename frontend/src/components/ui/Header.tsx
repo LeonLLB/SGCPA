79f6d62e8682e9b9bed0c17a8616d6c4a06525bb
Leonard Velásquez <leonard.avp@gmail.com>
2022-10-22 18:08:32 -0400

    Header y sidebar funcionales

--- frontend/src/components/ui/Header.tsx
+++ frontend/src/components/ui/Header.tsx
@@ -15,7 +15,7 @@ const Header = () => {
   return (
     <header className="z-50 bg-blue-500 flex flex-row w-full">
         <button onClick={toggleSidebar}>
-            <span className="material-icons text-4xl">menu</span>
+            <span className="material-icons !text-2xl">menu</span>
         </button>
         <div className="flex flex-row w-full items-center justify-center">
             <h1 className="text-lg sm:text-xl text-center md:text-2xl">Sistema de Gestión y Control de Proyectos Académicos SGCPA</h1>
--- frontend/src/components/ui/Sidebar.tsx
+++ frontend/src/components/ui/Sidebar.tsx
@@ -3,8 +3,8 @@ import useElementAsyncTransition from "../../hooks/useElementAsyncTransition";
 import SidebarGroupItemHeader from "./SidebarGroupItemHeader";
 import SidebarItem from "./SidebarItem";
 
-const Sidebar = ({closing, onClose}:{closing:boolean, onClose: ()=>void}) => {
-	
+const Sidebar = ({ closing, onClose }: { closing: boolean, onClose: () => void }) => {
+
 	const estudiantesItemState = useElementAsyncTransition(300)
 	const docentesItemState = useElementAsyncTransition(300)
 	const cargasItemState = useElementAsyncTransition(300)
@@ -13,81 +13,81 @@ const Sidebar = ({closing, onClose}:{closing:boolean, onClose: ()=>void}) => {
 	const proyectosItemState = useElementAsyncTransition(300)
 	const adminItemState = useElementAsyncTransition(300)
 
-	const onSidebarClose = (event:MouseEvent) => {
+	const onSidebarClose = (event: MouseEvent) => {
 		event.preventDefault();
 		onClose()
 	}
 
-	const onSidebarItemClick = (link : string) => {
-    // router.push(link)
-    onClose()
-  }
-	
-  return (
-    <div>
-			<div onClick={onSidebarClose} className={` modal-background ${closing? "opacity-0 bg-transparent closing-modal":"opacity-50 bg-gray-900 opening-modal"}`}></div>
-			
-			<div className={`fixed top-15 sm:top-11 transition-all duration-300 bg-gray-200 rounded-tr-xl rounded-br-xl z-50 flex flex-col w-52 h-[94vh] sm:h-[95.25vh] ${closing? "-translate-x-52":"opening-sidebar-body"}`}>
-					<div className="flex flex-col overflow-y-auto p-4 divide-y-2 divide-gray-300">
-            <SidebarItem isActive={false} icon="home" title="Principal" onClick={()=>{onSidebarItemClick("/")}}/>
-						<SidebarItem isActive={false} icon="school" title="Estudiantes" onClick={estudiantesItemState.Interaction}/>
-						{	estudiantesItemState.Visible &&
-							<SidebarGroupItemHeader closing={estudiantesItemState.Closing}>
-								<SidebarItem isActive={false} icon="school" title="Listado" onClick={()=>{onSidebarItemClick("/estudiantes")}}/>
-								<SidebarItem isActive={false} icon="add" title="Añadir" onClick={()=>{onSidebarItemClick("/estudiantes/registrar")}}/>
-							</SidebarGroupItemHeader>
-						}
-						<SidebarItem isActive={false} icon="person" title="Docentes" onClick={docentesItemState.Interaction}/>
-						{	docentesItemState.Visible &&
-							<SidebarGroupItemHeader closing={docentesItemState.Closing}>
-								<SidebarItem isActive={false} icon="person" title="Listado" onClick={()=>{onSidebarItemClick("/docentes")}}/>
-								<SidebarItem isActive={false} icon="person_add" title="Añadir" onClick={()=>{onSidebarItemClick("/docentes/registrar")}}/>
-							</SidebarGroupItemHeader>
-						}
-						<SidebarItem isActive={false} icon="calendar_month" title="Cargas Académicas" onClick={cargasItemState.Interaction}/>
-						{	cargasItemState.Visible &&
-							<SidebarGroupItemHeader closing={cargasItemState.Closing}>
-								<SidebarItem isActive={false} icon="calendar_month" title="Listado" onClick={()=>{onSidebarItemClick("/cargas")}}/>
-								<SidebarItem isActive={false} icon="add" title="Añadir" onClick={()=>{onSidebarItemClick("/cargas/registrar")}}/>
-								<SidebarItem icon="picture_as_pdf" title="Generar PDF" tiny={true} />
-							</SidebarGroupItemHeader>
-						}
-						<SidebarItem isActive={false} icon="badge" title="Comites Evaluadores" onClick={comitesItemState.Interaction}/>
-						{	comitesItemState.Visible &&
-							<SidebarGroupItemHeader closing={comitesItemState.Closing}>
-								<SidebarItem isActive={false} icon="badge" title="Listado" onClick={()=>{onSidebarItemClick("/comites")}}/>
-								<SidebarItem icon="picture_as_pdf" title="Generar PDF" tiny={true} />
-							</SidebarGroupItemHeader>
-						}
-						<SidebarItem isActive={false} icon="co_present" title="Presentaciones" onClick={presentacionesItemState.Interaction}/>
-						{	presentacionesItemState.Visible &&
-							<SidebarGroupItemHeader closing={presentacionesItemState.Closing}>
-								<SidebarItem isActive={false} icon="co_present" title="Listado" onClick={()=>{onSidebarItemClick("/presentaciones")}}/>
-								<SidebarItem isActive={false} icon="add" title="Añadir" onClick={()=>{onSidebarItemClick("/presentaciones/registrar")}}/>
-								<SidebarItem icon="picture_as_pdf" title="Generar PDF" tiny={true} />
-							</SidebarGroupItemHeader>
-						}
-						<SidebarItem isActive={false} icon="book" title="Proyectos" onClick={proyectosItemState.Interaction}/>
-						{	proyectosItemState.Visible &&
-							<SidebarGroupItemHeader closing={proyectosItemState.Closing}>
-								<SidebarItem isActive={false} icon="book" title="Listado" onClick={()=>{onSidebarItemClick("/proyectos")}}/>
-								<SidebarItem isActive={false} icon="location_on" title="Comunidades abordadas" tiny={true} onClick={()=>{onSidebarItemClick("/proyectos")}}/>
-								<SidebarItem isActive={false} icon="add" title="Añadir" onClick={()=>{onSidebarItemClick("/")}}/>
-								<SidebarItem icon="picture_as_pdf" title="Generar PDF" tiny={true} />
-							</SidebarGroupItemHeader>
-						}
-						<SidebarItem isActive={false} icon="description" title="Formatos" onClick={()=>{onSidebarItemClick("/formatos")}}/>
-						<SidebarItem isActive={false} icon="settings" title="Administrativo" onClick={adminItemState.Interaction}/>
-            {	adminItemState.Visible &&
-							<SidebarGroupItemHeader closing={adminItemState.Closing}>
-								<SidebarItem isActive={false} icon="school" title="PNF" onClick={()=>{onSidebarItemClick("/admin/pnf")}}/>
-								<SidebarItem icon="backup" title="Respaldo"/>
-							</SidebarGroupItemHeader>
-						}
-          </div>
+	const onSidebarItemClick = (link: string) => {
+		// router.push(link)
+		onClose()
+	}
+
+	return (
+		<div>
+			<div onClick={onSidebarClose} className={` modal-background ${closing ? "opacity-0 bg-transparent closing-modal" : "opacity-50 bg-gray-900 opening-modal"}`}></div>
+
+			<div className={`fixed top-8 transition-all duration-300 bg-gray-200 rounded-tr-xl rounded-br-xl z-50 flex flex-col w-52 h-[94vh] sm:h-[95.25vh] ${closing ? "-translate-x-52" : "opening-sidebar-body"}`}>
+				<div className="flex flex-col overflow-y-auto p-4 divide-y-2 divide-gray-300">
+					<SidebarItem isActive={false} icon="home" title="Principal" onClick={() => { onSidebarItemClick("/") }} />
+					<SidebarItem isActive={false} icon="school" title="Estudiantes" onClick={estudiantesItemState.Interaction} />
+					{estudiantesItemState.Visible &&
+						<SidebarGroupItemHeader closing={estudiantesItemState.Closing}>
+							<SidebarItem isActive={false} icon="school" title="Listado" onClick={() => { onSidebarItemClick("/estudiantes") }} />
+							<SidebarItem isActive={false} icon="add" title="Añadir" onClick={() => { onSidebarItemClick("/estudiantes/registrar") }} />
+						</SidebarGroupItemHeader>
+					}
+					<SidebarItem isActive={false} icon="person" title="Docentes" onClick={docentesItemState.Interaction} />
+					{docentesItemState.Visible &&
+						<SidebarGroupItemHeader closing={docentesItemState.Closing}>
+							<SidebarItem isActive={false} icon="person" title="Listado" onClick={() => { onSidebarItemClick("/docentes") }} />
+							<SidebarItem isActive={false} icon="person_add" title="Añadir" onClick={() => { onSidebarItemClick("/docentes/registrar") }} />
+						</SidebarGroupItemHeader>
+					}
+					<SidebarItem isActive={false} icon="calendar_month" title="Cargas Académicas" onClick={cargasItemState.Interaction} />
+					{cargasItemState.Visible &&
+						<SidebarGroupItemHeader closing={cargasItemState.Closing}>
+							<SidebarItem isActive={false} icon="calendar_month" title="Listado" onClick={() => { onSidebarItemClick("/cargas") }} />
+							<SidebarItem isActive={false} icon="add" title="Añadir" onClick={() => { onSidebarItemClick("/cargas/registrar") }} />
+							<SidebarItem icon="picture_as_pdf" title="Generar PDF" tiny={true} />
+						</SidebarGroupItemHeader>
+					}
+					<SidebarItem isActive={false} icon="badge" title="Comites Evaluadores" onClick={comitesItemState.Interaction} />
+					{comitesItemState.Visible &&
+						<SidebarGroupItemHeader closing={comitesItemState.Closing}>
+							<SidebarItem isActive={false} icon="badge" title="Listado" onClick={() => { onSidebarItemClick("/comites") }} />
+							<SidebarItem icon="picture_as_pdf" title="Generar PDF" tiny={true} />
+						</SidebarGroupItemHeader>
+					}
+					<SidebarItem isActive={false} icon="co_present" title="Presentaciones" onClick={presentacionesItemState.Interaction} />
+					{presentacionesItemState.Visible &&
+						<SidebarGroupItemHeader closing={presentacionesItemState.Closing}>
+							<SidebarItem isActive={false} icon="co_present" title="Listado" onClick={() => { onSidebarItemClick("/presentaciones") }} />
+							<SidebarItem isActive={false} icon="add" title="Añadir" onClick={() => { onSidebarItemClick("/presentaciones/registrar") }} />
+							<SidebarItem icon="picture_as_pdf" title="Generar PDF" tiny={true} />
+						</SidebarGroupItemHeader>
+					}
+					<SidebarItem isActive={false} icon="book" title="Proyectos" onClick={proyectosItemState.Interaction} />
+					{proyectosItemState.Visible &&
+						<SidebarGroupItemHeader closing={proyectosItemState.Closing}>
+							<SidebarItem isActive={false} icon="book" title="Listado" onClick={() => { onSidebarItemClick("/proyectos") }} />
+							<SidebarItem isActive={false} icon="location_on" title="Comunidades abordadas" tiny={true} onClick={() => { onSidebarItemClick("/proyectos") }} />
+							<SidebarItem isActive={false} icon="add" title="Añadir" onClick={() => { onSidebarItemClick("/") }} />
+							<SidebarItem icon="picture_as_pdf" title="Generar PDF" tiny={true} />
+						</SidebarGroupItemHeader>
+					}
+					<SidebarItem isActive={false} icon="description" title="Formatos" onClick={() => { onSidebarItemClick("/formatos") }} />
+					<SidebarItem isActive={false} icon="settings" title="Administrativo" onClick={adminItemState.Interaction} />
+					{adminItemState.Visible &&
+						<SidebarGroupItemHeader closing={adminItemState.Closing}>
+							<SidebarItem isActive={false} icon="school" title="PNF" onClick={() => { onSidebarItemClick("/admin/pnf") }} />
+							<SidebarItem icon="backup" title="Respaldo" />
+						</SidebarGroupItemHeader>
+					}
+				</div>
 			</div>
-    </div>
-  )
+		</div>
+	)
 }
 
 export default Sidebar
